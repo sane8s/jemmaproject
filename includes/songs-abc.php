9fa726cfe03d6d1e37985ecafaf68b65ac8a4a77
sane8s <saneyates@gmail.com>
2022-01-02 18:17:31 -0600

    Add files
    updated text size on filter letters

--- includes/songs-abc.php
+++ includes/songs-abc.php
@@ -1,15 +1,15 @@
 <?php
 include 'db/dbconnection.php';
 
-$query = "SELECT upper(SUBSTRING(song_title, 1, 1)) AS letter FROM song GROUP BY letter";
+$query = "SELECT upper(SUBSTRING(song_title, 1, 1)) AS letter FROM song GROUP BY letter ORDER BY letter";
 $result = mysqli_query($link, $query);
 
-echo "<ul class='list-inline'><li class='list-inline-item'><a href='songs.php'>ALL</a></li>";
+echo "<ul class='list-inline'><li class='list-inline-item p-1 h5'><a href='songs.php'>ALL</a></li>";
 if (mysqli_num_rows($result) > 0){
   /*output data of each row*/
   while($row = mysqli_fetch_assoc($result)){
     $l = $row["letter"];
-    echo "<li class='list-inline-item'><a href='songs.php?id=$l'>" . $l . "</a></li>";    
+    echo "<li class='list-inline-item p-1 h5'><a href='songs.php?id=$l'>" . $l . "</a></li>";    
   
   }
 } else {
--- includes/words-abc.php
+++ includes/words-abc.php
@@ -4,12 +4,12 @@ include 'db/dbconnection.php';
 $query = "SELECT upper(SUBSTRING(word_name, 1, 1)) AS letter FROM word GROUP BY letter ORDER BY letter";
 $result = mysqli_query($link, $query);
 
-echo "<ul class='list-inline'><li class='list-inline-item'><a href='index.php'>ALL</a></li>";
+echo "<ul class='list-inline '><li class='list-inline-item p-1 h5'><a href='index.php'>ALL</a></li>";
 if (mysqli_num_rows($result) > 0){
   /*output data of each row*/
   while($row = mysqli_fetch_assoc($result)){
     $l = $row["letter"];
-    echo "<li class='list-inline-item'><a href='index.php?id=$l'>" . $l . "</a></li>";
+    echo "<li class='list-inline-item p-1 h5'><a href='index.php?id=$l'>" . $l . "</a></li>";
  
   }
 } else {
