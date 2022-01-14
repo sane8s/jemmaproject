90e05944fa8c5114fb0ad50283e7bb53b6f3488f
sane8s <saneyates@gmail.com>
2022-01-14 09:53:20 -0600

    Add files via upload
    Added analytics script.

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <?php include "db/ganalytics.html";?>
   <!--<script>
 function check(sort) {
   document.getElementById("sortOption").value=sort;
@@ -18,12 +19,12 @@ function check(sort) {
     <!-- Site header -->    
         <div class="container">
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-              <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+              <a href="/jemmaproject" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                 <h3><span class="badge bg-dark">JP</span></h3>
                 <span class="fs-4 px-4">Jemmaproject</span>
               </a>        
               <ul class="nav nav-pills">
-                <li class="nav-item"><a href="/" class="nav-link">Words</a></li>
+                <li class="nav-item"><a href="/jemmaproject" class="nav-link">Words</a></li>
                 <li class="nav-item"><a href="songs.php" class="nav-link">Songs</a></li>
                 <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
                 <li class="nav-item"><a href="archives.php" class="nav-link">Archives</a></li>
